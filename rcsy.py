0c6cd7fc30bbd33280b35cd7c2f04a3a623681e7
ken-collab <69073190+ken-collab@users.noreply.github.com>
2022-04-17 16:13:03 +0800

    Updata rcsy.py
    Add write something into  the file with rcsy

--- rcsy.py
+++ rcsy.py
@@ -1,1 +1,7 @@
-
+#<class '_io.TextIOWrapper'>
+def into(filename, mode):
+    global rcsy_data
+    rcsy_data = open(filename, mode)
+def write_rcsy(name, text):
+    rcsy_data.write(name + "--" + text + "\n")
+    rcsy_data.flush()
